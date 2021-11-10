2bffa483cb9dccd4ef26296be686eb52a04ce3b7
zhou <zhoushaofeng@mail.ustc.edu.cn>
2021-11-10 20:37:36 +0800

    lab7 section2

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,8 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t mutex[NBUCKET];
+
 double
 now()
 {
@@ -42,6 +44,7 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
+	pthread_mutex_lock(&mutex[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
@@ -53,6 +56,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+	pthread_mutex_unlock(&mutex[i]);
 }
 
 static struct entry*
@@ -99,6 +103,8 @@ get_thread(void *xa)
 int
 main(int argc, char *argv[])
 {
+	for(int i=0;i<NBUCKET;i++)
+  	pthread_mutex_init(&mutex[i],NULL);
   pthread_t *tha;
   void *value;
   double t1, t0;
