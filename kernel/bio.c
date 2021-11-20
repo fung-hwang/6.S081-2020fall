3e4b4adbe2b1b0abaaee8a8ed2a0c97c6fd1455a
zhou <zhoushaofeng@mail.ustc.edu.cn>
2021-11-20 09:13:25 +0800

    updata lab8

--- kernel/bio.c
+++ kernel/bio.c
@@ -13,8 +13,6 @@
 // * Only one process at a time can use a buffer,
 //     so do not keep them longer than necessary.
 
-#define HASH(id) (id % NBUCKET)
-
 #include "types.h"
 #include "param.h"
 #include "spinlock.h"
@@ -69,84 +67,6 @@ binit(void)
 static struct buf*
 bget(uint dev, uint blockno)
 {
-/*
-	struct buf* b;
-
-	int bid = HASH(blockno);
-	acquire(&bcache.hashtable[bid].lock);
-
-	// Is the block already cached?
-	for (b = bcache.hashtable[bid].head.next; b != &bcache.hashtable[bid].head; b = b->next) {
-		if (b->dev == dev && b->blockno == blockno) {
-			b->refcnt++;
-
-			// 记录使用时间戳
-			acquire(&tickslock);
-			b->ticks = ticks;
-			release(&tickslock);
-
-			release(&bcache.hashtable[bid].lock);
-			acquiresleep(&b->lock);
-			return b;
-		}
-	}
-
-	// Not cached.
-	b = 0;
-	struct buf* tmp;
-
-	// Recycle the least recently used (LRU) unused buffer.
-	// 从当前散列桶开始查找
-	for (int i = bid, cycle = 0; cycle != NBUCKET; i = (i + 1) % NBUCKET) {
-		++cycle;
-		// 如果遍历到当前散列桶，则不重新获取锁
-		if (i != bid) {
-			if (!holding(&bcache.hashtable[i].lock))
-				acquire(&bcache.hashtable[i].lock);
-			else
-				continue;
-		}
-
-		for (tmp = bcache.hashtable[i].head.next; tmp != &bcache.hashtable[i].head; tmp = tmp->next)
-			// 使用时间戳进行LRU算法，而不是根据结点在链表中的位置
-			if (tmp->refcnt == 0 && (b == 0 || tmp->ticks < b->ticks))
-				b = tmp;
-
-		if (b) {
-			// 如果是从其他散列桶窃取的，则将其以头插法插入到当前桶
-			if (i != bid) {
-				b->next->prev = b->prev;
-				b->prev->next = b->next;
-				release(&bcache.hashtable[i].lock);
-
-				b->next = bcache.hashtable[bid].head.next;
-				b->prev = &bcache.hashtable[bid].head;
-				bcache.hashtable[bid].head.next->prev = b;
-				bcache.hashtable[bid].head.next = b;
-			}
-
-			b->dev = dev;
-			b->blockno = blockno;
-			b->valid = 0;
-			b->refcnt = 1;
-
-			acquire(&tickslock);
-			b->ticks = ticks;
-			release(&tickslock);
-
-			release(&bcache.hashtable[bid].lock);
-			acquiresleep(&b->lock);
-			return b;
-		}
-		else {
-			// 在当前散列桶中未找到，则直接释放锁
-			if (i != bid)
-				release(&bcache.hashtable[i].lock);
-		}
-	}
-
-	panic("bget: no buffers");
-	*/
 	struct buf* b;
 
 	int busketno = blockno % NBUCKET;
